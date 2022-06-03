60728e3132e326a5c8a0555af41dc68e8024d1f5
jordan-sergo <105432646+jordan-sergo@users.noreply.github.com>
2022-06-03 12:13:28 +0200

    Add files via upload

---
+++ pongasticno/setup.py
@@ -0,0 +1,32 @@
+import pygame
+pygame.init()
+
+WIDTH, HEIGHT = 700, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(("pong"))
+
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+def draw(win):
+    win.fill(WHITE)
+    pygame.display.update()
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+        draw(WIN)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        
+    pygame.quit()        
+if __name__ == "__main__":
+    main()
