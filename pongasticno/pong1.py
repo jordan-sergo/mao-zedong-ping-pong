65e6a3b7e1f2e2899131748a033500044ece5928
jordan-sergo <105432646+jordan-sergo@users.noreply.github.com>
2022-06-10 09:06:33 +0200

    Add files via upload

---
+++ pongasticno/pong1.py
@@ -0,0 +1,52 @@
+import pygame
+pygame.init()
+
+WIDTH, HEIGHT = 600, 400
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(("pong"))
+
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+class Paddle:
+    COLOR = WHITE
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x = x, self.y = y, self.width = width, self.height = height))    
+
+def draw(win, paddles):
+    win.fill(BLACK)
+
+    for paddle in paddles:
+        paddle.draw(win)
+
+    pygame.display.update()
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+    while run:
+        clock.tick(FPS)
+        draw(WIN, [left_paddle, right_paddle])
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        
+    pygame.quit()        
+if __name__ == "__main__":
+    main()
